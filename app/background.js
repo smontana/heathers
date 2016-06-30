a29620841108dfb0cf4fa4b33395771e3505e987
Stephen Montana <stephen.a.montana@gmail.com>
2016-06-29 19:06:19 -0500

    removed menu items

--- app/background.js
+++ app/background.js
@@ -3,42 +3,30 @@
 // It doesn't have any windows which you can see on screen, but we can open
 // window from here.
 
-import { app, Menu } from 'electron';
-import { devMenuTemplate } from './helpers/dev_menu_template';
-import { editMenuTemplate } from './helpers/edit_menu_template';
-import createWindow from './helpers/window';
+import { app, Menu } from 'electron'
+import createWindow from './helpers/window'
 
 // Special module holding environment variables which you declared
 // in config/env_xxx.json file.
-import env from './env';
+import env from './env'
 
-var mainWindow;
-
-var setApplicationMenu = function () {
-  var menus = [editMenuTemplate];
-  if (env.name !== 'production') {
-    menus.push(devMenuTemplate);
-  }
-  Menu.setApplicationMenu(Menu.buildFromTemplate(menus));
-};
+var mainWindow
 
 app.on('ready', function () {
-  setApplicationMenu();
-
   var mainWindow = createWindow('main', {
     width: 1000,
     height: 600,
     titleBarStyle: 'hidden-inset',
     autoHideMenuBar: true
-  });
+  })
 
-  mainWindow.loadURL('file://' + __dirname + '/app.html');
+  mainWindow.loadURL('file://' + __dirname + '/app.html')
 
   if (env.name !== 'production') {
-    mainWindow.openDevTools();
+    mainWindow.openDevTools()
   }
-});
+})
 
 app.on('window-all-closed', function () {
-  app.quit();
-});
+  app.quit()
+})
